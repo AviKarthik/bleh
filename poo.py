71cc14c7cbc75b794611de5758ae7272c00c01c0
AviKarthik <123848034+AviKarthik@users.noreply.github.com>
2026-03-25 15:14:09 -0700

    Refactor Python script to use one-liner execution

--- poo.py
+++ poo.py
@@ -1,6 +1,1 @@
-python - << 'PY'
-from ai_edge_litert.interpreter import Interpreter
-i = Interpreter(model_path="/home/mvrop-avi/model.tflite")
-i.allocate_tensors()
-print(i.get_input_details())
-PY
+python -c "from ai_edge_litert.interpreter import Interpreter; i=Interpreter(model_path='/home/mvrop-avi/model_full_int8.tflite'); i.allocate_tensors(); print(i.get_input_details())"
